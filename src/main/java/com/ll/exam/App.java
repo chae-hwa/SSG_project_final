8675ce2fb28d4fcc107b88078ff97e4759ae1e98
chae-hwa <ihj0519@naver.com>
2022-07-16 05:47:16 +0900

    5단계

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,17 +1,18 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     Scanner sc;
     int wiseSayingLastId;
-
-
+    List<WiseSaying> wiseSayings;
 
     App(){
     sc = new Scanner(System.in);
     wiseSayingLastId = 0;
-
+    wiseSayings = new ArrayList<>();
 
     }
     public void run() {
@@ -26,9 +27,22 @@ public class App {
                 case "등록":
                     write();
                     break;
+                case "목록":
+                    list();
+                    break;
                 case "종료":
                     break outer;
             }
+        }
+    }
+
+    private void list() {
+        System.out.println("번호 / 명언 / 작가");
+        System.out.println("----------------");
+
+        for(int i = wiseSayings.size() - 1; i>=0; i--){
+            WiseSaying wiseSaying_ = wiseSayings.get(i);
+            System.out.printf("%d / %s / %s\n",wiseSaying_.id,wiseSaying_.content,wiseSaying_.author);
         }
     }
 
@@ -39,8 +53,9 @@ public class App {
         String author = sc.nextLine().trim();
 
         int id = ++wiseSayingLastId;
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n",id);
-
     }
 }
